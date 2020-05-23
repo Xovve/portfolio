63a7f0a6ae0dfa1731fad4bb4c672d7b7ba62de1
xovve <33521478+Xovve@users.noreply.github.com>
2020-05-23 23:39:12 +0200

    Fix Vanta.js bug, add new header background

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -4,16 +4,24 @@ import { getTranslation } from "../LangSwitcher";
 import { Link } from "react-scroll";
 import Navbar from "../Navbar";
 import "../../styles/header.scss";
+import TRUNK from 'vanta/dist/vanta.trunk.min';
 
 class Header extends React.Component {
   componentDidMount() {
-    this.effect = window.VANTA.DOTS({
+    this.effect = TRUNK({
       el: "#header",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.50,
+      scaleMobile: 1.00,
+      spacing: 15,
+      chaos: 2.00,
       color: 0xee5b2c,
       color2: 0xee5b2c,
       backgroundColor: 0x001234,
-      size: 5.6,
-      spacing: 34.0
+
     });
   }
   componentWillUnmount() {
